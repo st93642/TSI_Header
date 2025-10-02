e0d3bbc6c582822e3e6ff4c37dcd496405303af2
st93642 <st93642@students.tsi.lv>
2025-10-02 17:12:40 +0300

    Clean up

--- learn_exercises/c/input_output_c.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    // TODO: read two integers using scanf
-
-    // TODO: calculate the sum, difference (first - second), and product
-    // TODO: print each result on its own line using printf
-    return 0;
-}
